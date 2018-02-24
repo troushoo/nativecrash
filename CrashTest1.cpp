cc9d2b05247d1f070f2323c117d96c4bb56b9b0e
troushoo <troushoo@gmail.com>
2018-02-24 23:02:03 +0900

    Create CrashTest1.cpp

---
+++ CrashTest1.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+
+
+int func3() {
+	int i = 0;
+	int j = 100;
+
+	printf("%d", j / i);
+	return 0;
+}
+
+int func2() {
+	func3();
+	return 0;
+}
+
+int func1() {
+	func2();
+	return 0;
+}
+
+int main()
+{
+	func1();
+    return 0;
+}
+
